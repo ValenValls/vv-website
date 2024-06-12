b39879e5511cffda1a58d02c9805df67f1288f15
ValenValls <valentinvalls@hotmail.com>
2024-06-12 13:39:34 -0300

    post_list cute

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,9 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user            
+            post.author = request.user      
+            if 'post' in request.POST:
+                post.published_date = timezone.now()                   
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -49,6 +51,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
