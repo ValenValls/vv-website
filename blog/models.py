7e2a87c8db11b1c5bb49dd6d7d28c8d318e83ea9
ValenValls <valentinvalls@hotmail.com>
2024-07-03 15:29:11 -0300

    moreperson

--- blog/models.py
+++ blog/models.py
@@ -3,25 +3,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 import google.generativeai as genai
+import os 
+import random
 
-token = ''
-genai.configure(api_key=token)
-
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='authored_posts')   
-    last_editor= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,blank=True, null=True, related_name='edited_posts')   
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-    photo = models.ImageField(upload_to='post-images', blank=True, null=True)
-    def publish(self):
-        self.published_date = timezone.now() 
-        self.save()       
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        model_id = "meta-llama/Meta-Llama-3-8B"
-        safety_settings = [
+google_token = os.getenv('GENAI_API_KEY')
+genai.configure(api_key=google_token)
+model_id = "meta-llama/Meta-Llama-3-8B"
+safety_settings = [
             {
                 "category": "HARM_CATEGORY_DANGEROUS",
                 "threshold": "BLOCK_NONE",
@@ -43,11 +31,35 @@ class Post(models.Model):
                 "threshold": "BLOCK_NONE",
             },
         ]
-        personalities = [["Genghis Khan, the mongol emperor","TheKhadKhan"], ["Julius Caesar, the Roman emperor", "IVeniVidiViciInYourM0M"]]
-        instruction = "You are " + personalities[0][0] +". You are looking at social media posts, which has a Title and Text, and you are writing a comment for the post."
+personalities = [["Genghis Khan, the mongol emperor","TheKhadKhan"], 
+                 ["Julius Caesar, the Roman emperor", "IVeniVidiViciInYourM0M"], 
+                 ["Shinzo Abe, japanese prime minister", "5hinz0Ab3"], 
+                 ["Mohandas Karamchand Gandhi","CIVBestNuker"], 
+                 ["Yukio Mishima, The Japanese author, poet, playwright, actor, model, Shintoist, nationalist, and founder of the Tatenokai", "MishimaThisBalls"],
+                ["Davinci, Italian inventor", "IpaintedLaMonaLisa"],
+                 ["Socrates, Greek Philosopher", "PPSmall=SmartBrain"],
+                ["Cleopatra, queen of egypt", "EgyptQueen"] ]
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='authored_posts')   
+    last_editor= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,blank=True, null=True, related_name='edited_posts')   
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    photo = models.ImageField(upload_to='post-images', blank=True, null=True)
+    def publish(self):
+        self.published_date = timezone.now() 
+        self.save()       
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+        personalitycom = random.randrange(len(personalities))
+        instruction = "You are " + personalities[personalitycom][0] +". You are looking at social media posts, which has a Title and Text, and you are writing a comment for the post."
         model = genai.GenerativeModel(model_name='gemini-1.5-flash', system_instruction= instruction, safety_settings = safety_settings)       
         response = model.generate_content("Title: " + self.title + ", Text: " + self.text)
-        Comment.objects.create(post=self, author=personalities[0][1], text=response.text, created_date = self.published_date)
+        Comment.objects.create(post=self, author=personalities[personalitycom][1], text=response.text, created_date = self.published_date)
 
     def __str__(self):
         return self.title
